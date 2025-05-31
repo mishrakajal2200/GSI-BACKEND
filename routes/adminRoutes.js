609788cbf878f38795b51c2a90196da649c5012c
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-05-31 12:33:59 -0500

    asd

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,14 +1,14 @@
-// routes/adminRoutes.js
-import express from 'express';
-import { adminLogin,getAdminStats } from '../controllers/adminController.js';
-import {authenticateUser,isAdmin} from '../middleware/authMiddleware.js';
+// // routes/adminRoutes.js
+// import express from 'express';
+// import { adminLogin,getAdminStats } from '../controllers/adminController.js';
+// import {authenticateUser,isAdmin} from '../middleware/authMiddleware.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/login', adminLogin);
+// router.post('/admin/login', adminLogin);
 
-router.get('/admin/stats', authenticateUser, isAdmin, getAdminStats);
+// router.get('/admin/stats', authenticateUser, isAdmin, getAdminStats);
 
 
 
-export default router;
+// export default router;
