c2e6cf1bf96ea8663db3c1c552e19b51dc5ec2d6
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-06-09 17:29:31 -0500

    smdfk

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -8,6 +8,10 @@ import  { authenticateUser,isAdmin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/adminlogin/login", adminLogin);
+router.post('/logout', (req, res) => {
+  res.clearCookie('token'); // or destroy session
+  res.status(200).json({ message: 'Logged out' });
+});
 router.get("/stats",authenticateUser, isAdmin, getAdminStats);
 router.get("/orders/recent", authenticateUser, isAdmin, getRecentOrders);
 router.patch("/orders/:id/status", authenticateUser, isAdmin, updateOrderStatus);
