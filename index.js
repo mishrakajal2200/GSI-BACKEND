a834aa7b66ee95db912597d5fc64ec7fec9721b1
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-07-13 07:17:42 -0500

    kdmfdlf

--- index.js
+++ index.js
@@ -351,9 +351,9 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // ✅ Serve React Frontend in Production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname, '../GSI-FRONTEND/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../GSI-FRONTEND/build', 'index.html'));
 });
 }
 
