6f1ed2466e6d7f4ca544a6ad34decdd003beeed7
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-08-28 06:21:41 -0500

    sdfsl

--- routes/quotationRoutes.js
+++ routes/quotationRoutes.js
@@ -7,18 +7,18 @@ import {
   acceptQuotation,
   rejectQuotation
 } from "../controllers/quotationController.js";
-import { protect, admin } from "../middleware/authMiddleware.js";
+import { authenticateUser, isAdmin} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Customer
-router.post("/", protect, createQuotation);         // request quotation
-router.get("/my", protect, getUserQuotations);      // get logged-in user's quotations
-router.put("/:id/accept", protect, acceptQuotation); // accept quotation
-router.put("/:id/reject", protect, rejectQuotation); // reject quotation
+router.post("/create", authenticateUser, createQuotation);         // request quotation
+router.get("/my", authenticateUser, getUserQuotations);      // get logged-in user's quotations
+router.put("/:id/accept", authenticateUser, acceptQuotation); // accept quotation
+router.put("/:id/reject", authenticateUser, rejectQuotation); // reject quotation
 
 // Admin
-router.get("/", protect, admin, getAllQuotations);   // list all quotations
-router.put("/:id/respond", protect, admin, respondQuotation); // admin sets price
+router.get("/", authenticateUser, isAdmin, getAllQuotations);   // list all quotations
+router.put("/:id/respond", authenticateUser, isAdmin, respondQuotation); // admin sets price
 
 export default router;
