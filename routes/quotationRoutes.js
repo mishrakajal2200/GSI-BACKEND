851ebece16a9121a399bb96299f3f110f0f2d4cb
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-08-29 16:24:12 -0500

    dfl

--- routes/quotationRoutes.js
+++ routes/quotationRoutes.js
@@ -12,7 +12,7 @@ import { authenticateUser, isAdmin} from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Customer
-router.post("/createquotation", authenticateUser, createQuotation);         // request quotation
+router.post("/create", authenticateUser, createQuotation);         // request quotation
 router.get("/my", authenticateUser, getUserQuotations);      // get logged-in user's quotations
 router.put("/:id/accept", authenticateUser, acceptQuotation); // accept quotation
 router.put("/:id/reject", authenticateUser, rejectQuotation); // reject quotation
