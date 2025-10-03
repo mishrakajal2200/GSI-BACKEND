c467b52975b94b1fac49b29b37c17a3bd7d2627b
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-10-03 17:28:17 -0500

    sdfld

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -291,7 +291,7 @@ export const placeCODOrder = async (req, res) => {
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_SECRET,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 export const createRazorpayOrder = async (req, res) => {
